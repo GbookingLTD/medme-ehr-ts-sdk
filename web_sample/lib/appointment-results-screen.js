d48adb3925d400c5e48479cac1513f63e5deaf56
swvitaliy <sw.vitaliy@gmail.com>
2019-07-25 21:27:45 +0300

    Add MedMe.env

--- web_sample/lib/appointment-results-screen.js
+++ web_sample/lib/appointment-results-screen.js
@@ -13,7 +13,7 @@ define('appointment-results-screen', ['handlebars',
 
             var appointmentResultLineTemplateFn = Handlebars.compile(appointmentResultLineTemplate);
             // infoklinika patient_id "10045940"
-            medmeApp.appointmentResultService.getPatientAppointmentResults(MedMe.env.PATIENT_ID, 10, 0, function(appointmentResults) {
+            medmeApp.appointmentResultService.getPatientAppointmentResults(medmeApp.env.PATIENT_ID, 10, 0, function(appointmentResults) {
                 appointmentResults.forEach(function(app) {
                     var html = appointmentResultLineTemplateFn(app);
                     document.getElementById('appointment-results-table-body')
--- web_sample/lib/prescription-screen.js
+++ web_sample/lib/prescription-screen.js
@@ -10,7 +10,7 @@ define('prescription-screen', ['handlebars',
             document.getElementById('mainContent').insertAdjacentHTML('beforeend', prescriptionTable);
 
             var prescriptionLineTemplateFn = Handlebars.compile(prescriptionLineTemplate);
-            medmeApp.prescriptionService.getPatientPrescriptions(MedMe.env.PATIENT_ID, 10, 0, function(prescriptions) {
+            medmeApp.prescriptionService.getPatientPrescriptions(medmeApp.env.PATIENT_ID, 10, 0, function(prescriptions) {
                 prescriptions.forEach(function(app) {
                     var html = prescriptionLineTemplateFn(app);
                     document.getElementById('prescriptions-table-body')
