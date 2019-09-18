653e1d2634b5df2cb6f3fc345756244686f7b690
nklv <nikita.nklv@gmail.com>
2019-09-18 10:57:53 +0300

    issue comment

--- src/types/ConnectionError.ts
+++ src/types/ConnectionError.ts
@@ -2,6 +2,7 @@ export class ConnectionError extends Error{
     __proto__: Error;
     constructor(){
         super("Connection cannot be established");
+        // https://github.com/Microsoft/TypeScript/issues/13965
         this.__proto__ = new.target.prototype;
     }
 }
