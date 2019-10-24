6cb66351fa732c69a0b153875a47d803d142bef4
nklv <nikita.nklv@gmail.com>
2019-10-24 15:31:30 +0300

    connection error test

--- tests/rpc.test.ts
+++ tests/rpc.test.ts
@@ -3,6 +3,7 @@ import {RpcErrorCodes} from "../src/services/RpcErrorCodes";
 import {IJsonRpcError} from "../src/services/jsonRPC/jsonRpcRequest";
 import {EHR_SERVER_ENDPOINT, login} from './login';
 import {Credentials} from "../src/services/Credentials";
+import {ConnectionError} from "../src/services/AuthService";
 
 var XMLHttpRequest = require('xhr2');
 
@@ -72,5 +73,15 @@ describe('Rpc', function () {
                 } else done(new Error('error'))
             })
         })
+
+        it('Connection error', function (done: (err?: any) => void){
+            let header = new JsonRPC.JsonRpcHeader("1", "api_info.healthcheck");
+            JsonRPC.Transports.xhr(EHR_SERVER_ENDPOINT, header, {}, function(err: any) {
+                if (err && err instanceof ConnectionError)
+                    done()
+                else
+                    done(new Error('error'))
+            });
+        });
     })
 })
