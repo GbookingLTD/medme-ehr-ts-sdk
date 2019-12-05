263a5b18802ee8ca40b95a6ba53a3087a2b825dc
root <root@gbooking.ru>
2019-12-05 17:12:23 +0000

    fix web_sample/lib/main.js

--- web_sample/lib/main.js
+++ web_sample/lib/main.js
@@ -22,7 +22,6 @@ requirejs([
     'env-info'
 ], function(auth, env, MedMe, envInfo) {
     envInfo.renderPanel(MedMe.EHR.SDK_VERSION, env);
-    return;
     auth.login(document.getElementById('mainContent'), function(authenticatedPatient) {
         if (authenticatedPatient)
             requirejs(['medme-app']);
