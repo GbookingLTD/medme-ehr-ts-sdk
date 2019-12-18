24555e4320489ba79148f3e05e582e642afb7392
swvitaliy <sw.vitaliy@gmail.com>
2019-12-18 17:32:03 +0300

    mode formatters l10n to separate files

--- src/formatters/SimpleTextFormatter.ts
+++ src/formatters/SimpleTextFormatter.ts
@@ -10,7 +10,7 @@ import { Period } from "../types/Period";
 import { DiagnosticReportModel } from "../models/DiagnosticReportModel";
 import { Observation } from "../types/Observation";
 import {Medication} from "../types/Medication";
-
+import l10n from "./l10n"
 
 function alignStrings(obj: object, keys: string[]) {
     // find max strings length
@@ -61,73 +61,7 @@ function dateFormat(d: Date): string {
 
 export class SimpleTextFormatter {
     public static LOCALIZE = {
-        "ru-ru": {
-            "MINUTE_UNIT": "мин.",
-            "YES": "Да",
-            "NO": "Нет",
-            "CREATED": "Дата создания",
-            "appointmentResult": {
-                "created": "Дата создания",
-                "start": "Дата и время начала",
-                "doctor": "Врач",
-                "duration": "Длительность",
-                "anamnesis": "Анамнез",
-                "medicalExaminationResult": "Результаты обследования",
-                "diagnosis": "Диагноз",
-                "recommendations": "Рекомендации",
-                "scheduledProcedures": "Назначенные на приеме процедуры, анализы, исследования",
-                "prescriptions": "Медикаментозные назначения (выписанные лекарства)",
-            },
-            "procedure": {
-                "created": "Дата создания",
-                "title": "Название",
-                "services": "Услуги",
-                "type": "Тип",
-                "required": "Обязательно",
-                "status": "Статус",
-                "period": "Предполагаемый период выполнения услуги",
-                "strictPeriod": "Период выполнения услуги, который нельзя нарушить",
-                "preparations": "Желаемые приготовления к процедуре",
-                "requiredPreparations": "Необходимые приготовления к процедуре",
-            },
-            "procedureType": [
-                "Рекомендация",
-                "Процедура",
-                "Анализы"
-            ],
-            "ProcedureExecStatus": [
-                "Запланировано",
-                "В процессе",
-                "Отменено",
-                "Выполнена"
-            ],
-            "Period": {
-                "begin": "Дата начала",
-                "end": "Дата окончания"
-            },
-            "DiagnosticReport": {
-                "Doctor": "Врач",
-                "EffectivePeriod": "Период, в течение которого данные пригодны",
-                "Result": "Результаты",
-                "Images": "Изображения",
-                "Attachments": "Документы"
-            },
-            "Prescription": {
-                "title": "Название",
-                "created": "Дата создания",
-                "recorderDoctor": "Врач, выписавший рецепт",
-                "medications": "Список лекарств",
-                "dosageText": "Дозировка",
-                "reasonText": "Причина назначения",
-                "validityPeriod": "Время, в течение которого рецепт действует",
-                "numberOfRepeats": "Сколько раз по этому рецепту можно получить лекарства",
-            },
-            "MedicationForm": {
-                0: "Порошок",
-                1: "Таблетки",
-                2: "Капсулы"
-            }
-        }
+        "ru-ru": l10n.ru
     };
 
     private _localize: object;
