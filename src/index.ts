e5cee19ee027f26c4b81f4179349cb1ea1470e8b
Vitaliy Sverchkov <vit@gbooking.ru>
2019-12-19 10:37:43 +0000

    bump sdk_version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import Formatters from "./formatters/index";
 import { Handlers } from './Handlers';
 
 export const EHR = {
-    SDK_VERSION: '1.5.6',
+    SDK_VERSION: '1.5.7',
     Types,
     Models,
     Services,
