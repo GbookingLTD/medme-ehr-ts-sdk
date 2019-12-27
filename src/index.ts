60a84401c510804492c364780a0c0f55b17d1c2d
nklv <nikita.nklv@gmail.com>
2019-12-27 17:52:52 +0700

    version update

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import Formatters from "./formatters/index";
 import { Handlers } from './Handlers';
 
 export const EHR = {
-    SDK_VERSION: '1.7.2',
+    SDK_VERSION: '1.7.3',
     Types,
     Models,
     Services,
