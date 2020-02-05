5ffb6b122beff73e2d05f1b82e23f234be7fd5ca
nklv <nikita.nklv@gmail.com>
2020-02-05 17:06:50 +0700

    moved json to fixtures

---
+++ tests/fixtures.ts
@@ -0,0 +1,435 @@
+export const medialogDiagnosticReportJson = {
+    "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54",
+    "patientId": "160",
+    "status": 0,
+    "type": 0,
+    "category": "",
+    "effectivePeriod": {
+        "begin": "0001-01-01T00:00:00",
+        "end": "0001-01-01T00:00:00"
+    },
+    "issuedDate": "2017-11-30T08:40:44",
+    "result": [
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:SostoyanieMatki",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "SostoyanieMatki",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "0"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:Gidrosonografiya",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "Gidrosonografiya",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "1"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:SvobodnoProhodimaPr",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "SvobodnoProhodimaPr",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:ZatrudnenoProhodimaPr",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "ZatrudnenoProhodimaPr",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:NeprohodimaVIstmichesko",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "NeprohodimaVIstmichesko",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:NeprohodimaVAmpulyarnom",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "NeprohodimaVAmpulyarnom",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "True"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:NeprohodimaVIntersticia",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "NeprohodimaVIntersticia",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:MeshotchatoeObrazovanie",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "MeshotchatoeObrazovanie",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:PeritubarnyeSpaykiPr",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "PeritubarnyeSpaykiPr",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:SvobodnoProhodimaLv",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "SvobodnoProhodimaLv",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "True"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:ZatrudnenoProhodimaLv",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "ZatrudnenoProhodimaLv",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:NeprohodimaVIstmichesk1",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "NeprohodimaVIstmichesk1",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:NeprohodimaVAmpulyarno1",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "NeprohodimaVAmpulyarno1",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:NeprohodimaVInterstici1",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "NeprohodimaVInterstici1",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:MeshotchatoeObrazovanie1",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "MeshotchatoeObrazovanie1",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        },
+        {
+            "id": "MedMe.EHR.Providers.MedialogProvider.Entities.Hysterosalpingograph:54:PeritubarnyeSpaykiLv",
+            "createdDate": "2017-11-30T08:40:44",
+            "patientInfo": null,
+            "type": 1,
+            "observationKey": "PeritubarnyeSpaykiLv",
+            "status": 0,
+            "effectivePeriod": {
+                "begin": "0001-01-01T00:00:00",
+                "end": "0001-01-01T00:00:00"
+            },
+            "issuedDate": "0001-01-01T00:00:00",
+            "performerDoctor": null,
+            "performerBusiness": null,
+            "value": {
+                "serializedValue": null,
+                "unit": 0,
+                "code": null,
+                "value": "False"
+            },
+            "note": null,
+            "device": null,
+            "interpretation": [],
+            "ranges": null,
+            "components": null
+        }
+    ],
+    "services": null,
+    "resultInterpreter": null,
+    "resultInterpretation": [],
+    "imagineMedia": [],
+    "attachments": []
+}
